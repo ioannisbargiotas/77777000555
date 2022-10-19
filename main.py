31ca09213ac24b999aee3f97643489f61471ee28
ioannisbargiotas <65168157+ioannisbargiotas@users.noreply.github.com>
2022-10-19 18:24:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -176,11 +176,11 @@ if __name__ == "__main__":
     maxnumPTS = X.shape[1]
     #
     # Number of features to consider at every split
-    max_features = [int(x) for x in np.linspace(1,np.ceil(np.round(0.7*maxnumPTS)),num = 10)]#np.arange(1,int(np.round(0.7*maxnumPTS)+1))#
+    max_features = [int(x) for x in np.linspace(1,np.ceil(np.round(maxnumPTS)),num = 10)]#np.arange(1,int(np.round(0.7*maxnumPTS)+1))#
     # Maximum number of levels in tree
-    max_depth = np.arange(1,8,2)
+    max_depth = np.arange(3,8,2)
     # Minimum number of samples required to split a node
-    min_samples_split = np.linspace(0, 0.2, num = 3)
+    min_samples_split = np.linspace(0.1, 0.3, num = 3)
     # Minimum number of samples required at each leaf node
     min_samples_leaf = [int(x) for x in np.linspace(1, int(np.round(0.2*maxMinLS)+1), num = 10)]#np.arange(1,int(np.round(0.2*maxMinLS)+1))#
     # Create the random grid
