fbfe19be205966606d2bea96f9648d6fc0431ca7
ioannisbargiotas <65168157+ioannisbargiotas@users.noreply.github.com>
2022-10-19 17:00:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def OOBAUC(estimator,X,y):
         #AUC[j] = (W1-n1*(n1+1)/2)/(n1*n2)
     
     "Error to be maximized"
-    Final_error = np.median(AUC)# - np.std(AUC)
+    Final_error = np.mean(AUC) - np.std(AUC)
     
     #"Calculate AUC"
     #PositiveOOB = PositiveOOBPosterior.mean(axis=1)
@@ -176,13 +176,13 @@ if __name__ == "__main__":
     maxnumPTS = X.shape[1]
     #
     # Number of features to consider at every split
-    max_features = [int(x) for x in np.linspace(1,np.ceil(np.round(0.7*maxnumPTS)),num = 5)]#np.arange(1,int(np.round(0.7*maxnumPTS)+1))#
+    max_features = [int(x) for x in np.linspace(1,np.ceil(np.round(0.7*maxnumPTS)),num = 10)]#np.arange(1,int(np.round(0.7*maxnumPTS)+1))#
     # Maximum number of levels in tree
     max_depth = np.arange(3,8,2)
     # Minimum number of samples required to split a node
     min_samples_split = np.linspace(0, 0.2, num = 3)
     # Minimum number of samples required at each leaf node
-    min_samples_leaf = [int(x) for x in np.linspace(1, int(np.round(0.2*maxMinLS)+1), num = 5)]#np.arange(1,int(np.round(0.2*maxMinLS)+1))#
+    min_samples_leaf = [int(x) for x in np.linspace(1, int(np.round(0.1*maxMinLS)+1), num = 10)]#np.arange(1,int(np.round(0.2*maxMinLS)+1))#
     # Create the random grid
     random_grid = {'max_features': max_features,
                    'max_depth': max_depth,
